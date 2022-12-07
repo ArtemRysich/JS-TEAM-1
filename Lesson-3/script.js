0086dc932696dc04e6e967aff00211d9678a565d
GalinaBespiata <116351519+GalinaBespiata@users.noreply.github.com>
2022-12-07 14:59:22 +0200

    задача 2

--- Lesson-3/script.js
+++ Lesson-3/script.js
@@ -1,10 +1,27 @@
 //Замінити всі розширення файлів з .css на .js
 
-const files = ['index.css', 'script.js', 'style.css', 'step-types.json', 'firebase.css'];
+// const files = ['index.css', 'script.js', 'style.css', 'step-types.json', 'firebase.css'];
 
-for (let i = 0; i < files.length; i += 1) {
-  console.log(files[i].replace('.css', '.js'));
-  console.log(files[i]);
-  files[i] = files[i].replace('.css', '.js');
+// for (let i = 0; i < files.length; i += 1) {
+//   console.log(files[i].replace('.css', '.js'));
+//   console.log(files[i]);
+//   files[i] = files[i].replace('.css', '.js');
+// }
+// console.log(files);
+
+//Створити функцію яка буде приймати 2 параметри
+//1 параметр це масив всіх юзерів. (users)
+//2 параметр це масив з чоловічими іменами. (mens)
+// Функція повина повертати масив з жіночими іменами.
+
+ const users = ['Artem', 'Anna', 'Larisa', 'Maksim', 'Svetlana', 'David', 'Roman', 'Olga'];
+ const men = ['Artem', 'Maksim', 'David', 'Roman'];
+const women=[];
+for( const user of users){
+if(!men.includes(user)){
+console.log(user)
+women.push(user) 
 }
-console.log(files);
+}
+console.log(women)
+
