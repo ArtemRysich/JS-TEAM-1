b545d22b23988aad0e4f1f89792aa655f5961d04
Yuliya N <y.sokolova.liya@gmail.com>
2022-12-15 16:40:32 +0200

    rewritten task

--- lesson-7/script.js
+++ lesson-7/script.js
@@ -32,7 +32,7 @@
 // Маємо масив з переможними комбінаціями (jackpot), а також декілька тестових масивів з ходами наших учасників.
 // Потрібно створити функція яка буде повертати true або false в залежності від того чи ходи учасника співпадають з однією з переможних комбінацій.
 
-/* const jackpot = [
+const jackpot = [
   [1, 2, 3],
   [1, 5, 9],
   [1, 4, 7],
@@ -46,23 +46,26 @@
 // перебираємо масив джекпот щоб отримати кожен вкладений всередину масив,
 // перебираємо потім кожен із вкладених масивів по елементам
 
-const winner = (arr) => {
-  let isWinner = false;
-  for (const elem of jackpot) {
-    let isAllArray = true;
-    for (const item of elem) {
-      if (!arr.includes(item)) {
-        isAllArray = false;
-        break;
-      }
-    }
-    if (isAllArray) {
-      isWinner = true;
-      return isWinner;
-    }
-  }
-  return isWinner;
-};
+const winner = (arr) =>
+  jackpot.some((item) => item.every((value) => arr.includes(value))); // value - елемент масива
+
+// const winner = (arr) => {
+//   let isWinner = false;
+//   for (const elem of jackpot) {
+//     let isAllArray = true;
+//     for (const item of elem) {
+//       if (!arr.includes(item)) {
+//         isAllArray = false;
+//         break;
+//       }
+//     }
+//     if (isAllArray) {
+//       isWinner = true;
+//       return isWinner;
+//     }
+//   }
+//   return isWinner;
+// };
 
 const player1 = [2, 4, 1]; //false
 const player2 = [9, 4, 3]; //false
@@ -70,41 +73,41 @@ const player3 = [6, 3, 9]; //true
 
 console.log(winner(player1));
 console.log(winner(player2));
-console.log(winner(player3)); */
+console.log(winner(player3));
 
 /* Визначати в кого була найтриваліша гра, повернути імя гравця */
-const players = [{
-        name: "Mango",
-        playtime: 1270,
-        gamesPlayed: 4
-    },
-    {
-        name: "Poly",
-        playtime: 469,
-        gamesPlayed: 2
-    },
-    {
-        name: "Ajax",
-        playtime: 690,
-        gamesPlayed: 3
-    },
-    {
-        name: "Kiwi",
-        playtime: 241,
-        gamesPlayed: 1
-    },
-];
-const longestPlay = players => {
-  let name = "";
-  let longestTime = 0;
-  for (const player of players) {
-    let play = player.playtime / player.gamesPlayed;
-    if (play > longestTime) {
-      longestTime = play;
-      name = player.name;
-    }
-  }
-  return name;
-}
-longestPlay(players);
-console.log(longestPlay(players))
+// const players = [{
+//         name: "Mango",
+//         playtime: 1270,
+//         gamesPlayed: 4
+//     },
+//     {
+//         name: "Poly",
+//         playtime: 469,
+//         gamesPlayed: 2
+//     },
+//     {
+//         name: "Ajax",
+//         playtime: 690,
+//         gamesPlayed: 3
+//     },
+//     {
+//         name: "Kiwi",
+//         playtime: 241,
+//         gamesPlayed: 1
+//     },
+// ];
+// const longestPlay = players => {
+//   let name = "";
+//   let longestTime = 0;
+//   for (const player of players) {
+//     let play = player.playtime / player.gamesPlayed;
+//     if (play > longestTime) {
+//       longestTime = play;
+//       name = player.name;
+//     }
+//   }
+//   return name;
+// }
+// longestPlay(players);
+// console.log(longestPlay(players))
