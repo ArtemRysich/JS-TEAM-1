0b8ed1ada20edeab8d688c245a7907c8d64999fb
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2022-12-15 15:51:44 +0200

    added function

---
+++ lesson-8/script.js
@@ -0,0 +1,42 @@
+const cars = [{
+    car: "Honda",
+    type: "Civic",
+    price: 12000,
+},
+    {
+        car: "Audi",
+        type: "Q7",
+        price: 40000,
+    },
+    {
+        car: "BMW",
+        type: "5 siries",
+        price: 9000,
+    },
+    {
+        car: "Honda",
+        type: "Accord",
+        price: 20000,
+    },
+    {
+        car: "Volvo",
+        type: "XC60",
+        price: 7000,
+    },
+];
+let theMostExpensiveCarPrice = 0
+let theMostExpensiveCarName = ''
+
+    cars.forEach(( {car, price}) => {
+        if(price > theMostExpensiveCarPrice){
+            theMostExpensiveCarPrice = price
+            theMostExpensiveCarName = car
+        }
+
+})
+console.log(theMostExpensiveCarName)
+
+// cars.sort((a,b) => b.price -  a.price)
+// theMostExpensiveCarPrice = cars[0].price
+// console.log(cars[0])
+// console.log(theMostExpensiveCarPrice)
