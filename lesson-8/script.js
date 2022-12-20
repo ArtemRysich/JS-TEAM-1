70be5b5be63fd92eac03f8ffb15188b9f60ec00a
GalinaBespiata <116351519+GalinaBespiata@users.noreply.github.com>
2022-12-20 16:35:52 +0200

    про Нарцисів

--- lesson-8/script.js
+++ lesson-8/script.js
@@ -46,3 +46,50 @@ console.log(carArr)
  */
 const carArr = cars.filter(({ price }) => price < 15000).map(({car}) => car)
 console.log(carArr)
+
+
+// ПРО НАРЦИСІВ___________
+// const people1 = [{ name: 'Alex', know: ['Eva', 'Jhon'], }, 
+//    { name: 'Ivan', know: ['Jhon', 'Alex'] }, 
+//   { name: 'Eva', know: ['Alex', 'Jhon'] },
+//   { name: 'Jhon', know: ['Alex'] },]
+    
+// const people2 = [{ name: 'Alex', know: ['Eva', 'Jhon'], },
+//   { name: 'Jhon', know: [] },
+//   { name: 'Eva', know: [] },
+//   { name: 'Ivan', know: ['Jhon', 'Alex'] },
+// ]; //// Expected result: Not found 
+
+// const people3 = [{ name: 'Alex', know: ['Eva', 'Jhon'], }, 
+// { name: 'Jhon', know: []  },
+// { name: 'Eva', know: ['Alex', 'Jhon'] },
+// {  name: 'Ivan', know: ['Jhon', 'Alex'] },  ]; //// Expected result: Jhon
+
+// const findNarcis = people2.filter(people => !people.know.length);
+ 
+// if (findNarcis.length === 1) {
+ 
+//   const narcis = people2.filter(people => people.know.length).every(people => people.know.includes(findNarcis[0].name));
+//  console.log(findNarcis[0].name) 
+// }
+// else {console.log("Нарцисів немає")}
+ 
+// findNarcis();
+
+const findNarcis = friends => {
+  const arr = friends.filter(friend => !friend.know.length);
+ 
+if (arr.length === 1) {
+  console.log(arr);
+ const name = arr[0].name;
+ const result = friends.every((friend) => friend.know.includes(name) || friend.name === name)
+ if(result){
+  console.log(name)
+ }else{
+  console.log("Нарцисів немає")
+ }
+ 
+}
+  else {console.log("Нарцисів немає")}
+}
+  findNarcis(people3);
