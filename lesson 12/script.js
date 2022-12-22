54a00c23dd60279b7857d6e3065f6c3d24f0d06f
Iryna <Iryna@MacBook-Air-MacBook.local>
2022-12-22 15:58:33 +0200

    ready

--- lesson 12/script.js
+++ lesson 12/script.js
@@ -1,40 +1,51 @@
-const square = document.querySelector(".square");
-// square.addEventListener('click', onClick);
-// let step = 0;
-// function onClick(evt) {
-//     step += 50;
-//     evt.currentTarget.style.marginLeft = step + "px"
-//     evt.currentTarget.style.marginTop = step + "px"
-//     console.log(evt.currentTarget)
+// const square = document.querySelector(".square");
+// // square.addEventListener('click', onClick);
+// // let step = 0;
+// // function onClick(evt) {
+// //     step += 50;
+// //     evt.currentTarget.style.marginLeft = step + "px"
+// //     evt.currentTarget.style.marginTop = step + "px"
+// //     console.log(evt.currentTarget)
+// // }
+
+// let stepX = 0;
+// let stepY = 0;
+
+// document.addEventListener("keydown", onPressArrow);
+// function onPressArrow(evt) {
+  
+//   switch (evt.code) {
+//     case "ArrowUp":
+        
+//         stepY = stepY - 10 >= 0 ? stepY - 10 : 0;
+       
+//           square.style.marginTop = stepY + "px";
+      
+//       break;
+//     case "ArrowDown":
+//         stepY += 10;
+//       square.style.marginTop = stepY + "px";
+//       break;
+//       case "ArrowRight":
+//         stepX += 10;
+//         square.style.marginLeft = stepX + "px";
+//         break;
+//         case "ArrowLeft":
+        
+//         stepX = stepX - 10 >= 0 ? stepX - 10 : 0;
+//         square.style.marginLeft = stepX + "px";
+
+//   }
+//   console.log(evt);
 // }
 
-let stepX = 0;
-let stepY = 0;
 
-document.addEventListener("keydown", onPressArrow);
-function onPressArrow(evt) {
-  
-  switch (evt.code) {
-    case "ArrowUp":
-        
-        stepY = stepY - 10 >= 0 ? stepY - 10 : 0;
-       
-          square.style.marginTop = stepY + "px";
-      
-      break;
-    case "ArrowDown":
-        stepY += 10;
-      square.style.marginTop = stepY + "px";
-      break;
-      case "ArrowRight":
-        stepX += 10;
-        square.style.marginLeft = stepX + "px";
-        break;
-        case "ArrowLeft":
-        
-        stepX = stepX - 10 >= 0 ? stepX - 10 : 0;
-        square.style.marginLeft = stepX + "px";
+const input = document.querySelector('.js-input')
+const title = document.querySelector('.js-title')
+input.addEventListener("input", onInput)
 
+function onInput(event) {
+  if (event.currentTarget.value.length > 15) {
+    title.textContent = 'Welcome'
   }
-  console.log(evt);
 }
