dcfa0a90e2d90bb36a4610389d1c1a384ec6812e
Iryna <Iryna@MacBook-Air-MacBook.local>
2022-12-29 17:15:43 +0200

    ready

--- lesson-15/script.js
+++ lesson-15/script.js
@@ -1,41 +1,56 @@
 const divEl = document.querySelector(".content");
-let stepX = [];
-let stepO = [];
+const restart = document.querySelector(".js-restart");
+let stepX = JSON.parse(localStorage.getItem("playerX")) || [];
+let stepO = JSON.parse(localStorage.getItem("playerO")) || [];
+console.log(stepX, stepO);
 let markup = "";
 for (let i = 1; i <= 9; i += 1) {
   markup += `<div class="item" data-id ="${i}"></div>`;
 }
 divEl.insertAdjacentHTML("beforeend", markup);
+[...divEl.children].forEach((item) => {
+  const id = Number(item.dataset.id);
+  console.log(item.dataset.id);
+  if (stepX.includes(id)) {
+    item.textContent = "X";
+  } else if (stepO.includes(id)) {
+    item.textContent = "O";
+  }
+});
 divEl.addEventListener("click", onClick);
-let pleyer = "X";
+restart.addEventListener("click", restartGame);
+let player = localStorage.getItem("currentPlayer") || "X";
 function onClick(evt) {
   if (!evt.target.textContent) {
     const { id } = evt.target.dataset;
     let isWinner = false;
-    evt.target.textContent = pleyer;
-    if (pleyer === "X") {
+    evt.target.textContent = player;
+    if (player === "X") {
       stepX.push(Number(id));
+      localStorage.setItem("playerX", JSON.stringify(stepX));
       console.log(stepX);
       isWinner = winner(stepX);
     } else {
       stepO.push(Number(id));
+      localStorage.setItem("playerO", JSON.stringify(stepO));
       console.log(stepO);
       isWinner = winner(stepO);
     }
     if (isWinner) {
-      console.log(pleyer + " is winner");
-      restartGame()
+      console.log(player + " is winner");
+      restartGame();
       return;
-
-    } 
-    pleyer = pleyer === "X" ? "O" : "X";
+    }
+    player = player === "X" ? "O" : "X";
+    localStorage.setItem("currentPlayer", player);
   }
 }
 function restartGame() {
-    divEl.innerHTML = markup;
-    stepO = []; 
-    stepX = []; 
-    pleyer = "X";
+  divEl.innerHTML = markup;
+  stepO = [];
+  stepX = [];
+  player = "X";
+  localStorage.clear();
 }
 const winner = (arr) =>
   jackpot.some((item) => item.every((value) => arr.includes(value))); // value - елемент масива
