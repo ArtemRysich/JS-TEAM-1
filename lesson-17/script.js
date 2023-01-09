6da813851e3dc92e30aad2abc59551b40e704015
GalinaBespiata <116351519+GalinaBespiata@users.noreply.github.com>
2023-01-09 15:41:23 +0200

    digitalClock

--- lesson-17/script.js
+++ lesson-17/script.js
@@ -2,11 +2,15 @@ const dateDay = document.querySelector(".js-date-day");
 const dateDate = document.querySelector(".js-date");
 const dateMonth = document.querySelector('.js-date-month');
 const dateYear = document.querySelector('.js-date-year');
+const digitalClock = document.querySelector(".js-digital-clock");
 
 const namesOfMonth = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень' , 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'];
 const namesOfDay = ['Неділя', 'Понеділок','Вівторок', 'Середа', 'Четвер', 'Пятниця', 'Субота' ]
 
-const currentDate = new Date();
+
+
+setInterval(()=>{
+    const currentDate = new Date();
 // console.log(currentDate);
 
 const dateCurrentDay = currentDate.getDay();
@@ -18,7 +22,14 @@ dateDay.textContent = namesOfDay[dateCurrentDay];
 dateDate.textContent = dateCurrentDate;
 dateMonth.textContent = namesOfMonth[dateCurrentMonth];
 dateYear.textContent = dateCurrentYear;
-
+ 
+const hours  = currentDate.getHours();
+const minutes  = currentDate.getMinutes();
+const seconds = currentDate.getSeconds();
+console.log(hours)
+const result = `Поточний час ${hours.toString().padStart(2, "0")} : ${minutes.toString().padStart(2, "0")} : ${seconds.toString().padStart(2, "0")}`;
+digitalClock.textContent = result;
+},1000)
 
 
 
